63fa8d1c7672e487d148a01ff5c409e4424e103f
jsweeney <jsweeney@skillstorm.com>
2026-03-25 14:17:38 -0500

    First commit - Spring initialized project

---
+++ src/main/java/com/skillstorm/jsweeney_proj1/JsweeneyProj1Application.java
@@ -0,0 +1,13 @@
+package com.skillstorm.jsweeney_proj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsweeneyProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsweeneyProj1Application.class, args);
+	}
+
+}
